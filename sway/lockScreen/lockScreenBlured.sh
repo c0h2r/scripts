a9c9b19d6fefe001cc570d3674534d6a4b22fb60
c0h2r <liaollib@gmail.com>
2020-05-05 23:29:14 +0300

    bckp

--- sway/lockScreen/lockScreenBlured.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-grim tmp.png
-blur_image tmp.png -o tmp_blured.png
-rm tmp.png
-swaylock  --image tmp_blured.png --indicator-radius 70 --indicator-thicknes 9 --inside-color 000 --line-color 000000 --ring-color 000000 --key-hl-color 000
-rm tmp_blured.png
